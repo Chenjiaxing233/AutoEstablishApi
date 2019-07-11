02bb6b4f573cdb5b9e07757d1ce529fc3054e6c3
chenjiaxing <627564618@qq.com>
2019-07-11 09:25:45 +0800

    1

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
 const http = require('http');
 const fs = require('fs');
 const commons = require('./common/commons')
+let loginUrl = '***'
+let pageUrl = '***'
 
-let pageUrl = 'http://192.168.1.169:8080/atom/v2/api-docs'
-
-http.get(pageUrl, function(res) {
-    var html = '';
-    res.on('data', function(data) {
-        html += data;
+http.get(loginUrl,(res)=>{
+    res.on('data',(data)=>{
+        console.log(data)
+        console.log(data.toString('utf8'));
     });
-    res.on('end', function() {
-        //console.log('html',html);
-        fs.readdir('./',function(err,file){
-            if(err){
-                throw err;
-            }
-            if(file.indexOf('api')==1){
-                //console.log('删除api')
-                commons.deleteall('./api')
-            }else{
-                fs.mkdir('./api',function(err){
-                    if(err){
-                        throw err;
-                    }
-                    //console.log('创建新文件夹成功！')
-                    let data = html.replace('<Json>','');
-                    data = data.replace('</Json>','')
-                    commons.write(data);
-                })
-            }
-        })
+    res.on('end',()=>{})
+})
+
+// http.get(pageUrl, function(res) {
+//     var html = '';
+//     res.on('data', function(data) {
+//         html += data;
+//     });
+//     res.on('end', function() {
+//         console.log('html',html);
+//         fs.readdir('./',function(err,file){
+//             if(err){
+//                 throw err;
+//             }
+//             console.log(file)
+//             if(file.indexOf('api')>-1){
+//                 console.log('删除api')
+//                 commons.deleteall('./api')
+//                 commons.writeApi(html)
+//             }else{
+//                 commons.writeApi(html)
+//             }
+//         })
     
-    });
-});
+//     });
+// });
